617340737515c562c8485591543738922865b946
gbsharing <gbsharing@yandex.ru>
2019-03-05 23:11:20 +0400

    Сделаны задания easy и normal

--- lesson05/home_work/hw05_easy.py
+++ lesson05/home_work/hw05_easy.py
@@ -1,10 +1,58 @@
+import os
+
+def create_folder(name):
+    dir_path = os.path.join(os.getcwd(), name)
+    try:
+        os.mkdir(dir_path)
+        print("Успешно создано")
+    except:
+        print("Невозможно создать")
+
+def list_of_folders():
+    file_path = os.getcwd()
+    dirs = []
+    for r, d, f in os.walk(file_path):
+        if r == file_path:
+            dirs = d
+    return dirs
+
+def delete_folder(name):
+    dir_path = os.path.join(os.getcwd(), name)
+    try:
+        os.removedirs(dir_path)
+        print("Успешно удалено")
+    except:
+        print("Невозможно удалить")
+
 # Задача-1:
 # Напишите скрипт, создающий директории dir_1 - dir_9 в папке,
 # из которой запущен данный скрипт.
 # И второй скрипт, удаляющий эти папки.
 
+if __name__ == "__main__":
+    for i in range(1, 10):
+        create_folder(f"dir_{i}")
+
+    print("В результате получились вот что:", list_of_folders())
+
+    input("Будем удалять... Нажмите Enter")
+
+    for i in range(1, 10):
+        delete_folder(f"dir_{i}")
+
+    print("В результате осталось вот что:", list_of_folders())
+    input()
+
 # Задача-2:
 # Напишите скрипт, отображающий папки текущей директории.
 
+if __name__ == "__main__":
+    print("В текущей директории следующие папки:", list_of_folders())
+
 # Задача-3:
 # Напишите скрипт, создающий копию файла, из которого запущен данный скрипт.
+
+if __name__ == "__main__":
+    import shutil
+    shutil.copy(__file__, os.path.abspath('file_copy.py'))
+    print("Исполняемый файл скопирован")
--- lesson05/home_work/hw05_normal.py
+++ lesson05/home_work/hw05_normal.py
@@ -13,3 +13,58 @@
 # Для решения данной задачи используйте алгоритмы из задания easy,
 # оформленные в виде соответствующих функций,
 # и импортированные в данный файл из easy.py
+
+import hw05_easy, sys, os
+
+file_path = os.getcwd()
+
+def print_help():
+    print("help - Получение справки")
+    print("cd - Перейти в папку")
+    print("lof - Просмотреть содержимое текущей папки")
+    print("df - Удалить папку")
+    print("cf - Создать папку")
+
+def change_dir():
+    name = input("Напишите название папки для перехода (пустая строка для перехода в начальную папку): ")
+    if name == "":
+        os.chdir(file_path)
+        print("Вы в начальной папке: ", file_path)
+    else:
+        dir_path = os.path.join(os.getcwd(), name)
+        try:
+            os.chdir(dir_path)
+            print("Успешно перешел")
+        except:
+            print("Невозможно перейти")
+
+def list_of_folder():
+    print(os.listdir(os.getcwd()))
+
+def delete_dir():
+    name = input("Напишите название папки для удаления: ")
+    hw05_easy.delete_folder(name)
+
+def create_dir():
+    name = input("Напишите название папки для создания: ")
+    hw05_easy.create_folder(name)
+
+do = {
+    "help": print_help,
+    "cd": change_dir,
+    "lof": list_of_folder,
+    "df": delete_dir,
+    "cf": create_dir
+}
+
+while True:
+    key = input("\nНапишите ключ действия или 'q' для выхода:\n")
+
+    if key == 'q':
+        sys.exit()
+    else:
+        if do.get(key):
+            do[key]()
+        else:
+            print("Задан неверный ключ")
+            print("Укажите ключ help для получения справки")
